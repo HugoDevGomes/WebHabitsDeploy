97d14c50228163716895b5e07bb21f301a35ed06
Hugo Gomes <hugogomeees13@hotmail.com>
2023-02-07 22:50:30 -0300

    add meta http

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AuthGoogleProvider } from './contexts/AuthContext';
 import { AppRoutes } from './routes/routes';
 import { SignIn } from './screens/Auth';
 import './styles/global.css';
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
 
 export function App() {
   return (
